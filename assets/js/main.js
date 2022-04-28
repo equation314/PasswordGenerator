10cc28fda10336ebc2b2f988763a6a203b7aa2bb
Yuekai Jia <equation618@gmail.com>
2022-04-28 14:03:34 +0800

    Update generator algorithm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,70 @@
-DOMAINS = ["tsinghua.edu.cn", "net9.org", "github.com", "Google", "Apple ID"];
+const DOMAINS = ["tsinghua.edu.cn", "net9.org", "github.com", "Google", "Apple ID"];
+const UPPERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERS = UPPERS.toLowerCase();
+const NUMBERS = "0123456789";
+const SPECIALS = "!#$%&()*@^";
+const LENGTH = 16;
 
-function genPassword() {
-  let domain = $("#input-domain")
-    .val()
-    .toLowerCase();
-  let rawPassword = $("#input-password").val();
+class Rng {
+  constructor(seed1, seed2) {
+    this.seed = seed1;
+    this.state = CryptoJS.HmacSHA3(seed2, seed1).toString(CryptoJS.enc.Hex);
+  }
+
+  nextWord() {
+    this.state = CryptoJS.HmacSHA3(this.seed, this.state).toString(CryptoJS.enc.Hex);
+    return parseInt('0x' + this.state.substr(0, 8), 16);
+  };
+
+  nextInt(start, end) {
+    if (end <= start)
+      return 0;
+    return start + this.nextWord() % (end - start);
+  };
+
+  nextChar(charset) {
+    return charset[this.nextInt(0, charset.length)];
+  };
+
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = this.nextInt(0, i + 1);
+      let tmp = array[j];
+      array[j] = array[i];
+      array[i] = tmp;
+    }
+  }
+}
+
+function genPassword(domain, rawPassword) {
   if (!domain || !rawPassword) {
     return;
   }
 
-  let hmac = CryptoJS.HmacSHA3(domain, rawPassword).toString(
-    CryptoJS.enc.Base64
-  );
-  let password = hmac.replace(/=/g, "").substr(-16);
+  let rng = new Rng(domain, rawPassword);
+  let password = [];
 
-  $("#input-result").val(password);
-  showPassword("#btn-eye-result");
-  setTimeout(() => hidePassword("#btn-eye-result"), 500);
-  $("#btn-copy").click();
+  password.push(rng.nextChar(UPPERS));
+  password.push(rng.nextChar(LOWERS));
+  password.push(rng.nextChar(NUMBERS));
+  password.push(rng.nextChar(SPECIALS));
+
+  for (let i = 0; i < LENGTH - 4; i++) {
+    let x = rng.nextInt(0, 10);
+    if (x < 3) {
+      password.push(rng.nextChar(UPPERS));
+    } else if (x < 6) {
+      password.push(rng.nextChar(LOWERS));
+    } else if (x < 8) {
+      password.push(rng.nextChar(NUMBERS));
+    } else if (x < 10) {
+      password.push(rng.nextChar(SPECIALS));
+    }
+  }
+
+  rng.shuffle(password);
+
+  return password.join("");
 }
 
 function chooseDomain(domain) {
@@ -51,7 +98,7 @@ function setTooltip(btn, message) {
 }
 
 function hideTooltip(btn) {
-  setTimeout(function() {
+  setTimeout(function () {
     $(btn)
       .tooltip("hide")
       .attr("data-original-title", "");
@@ -73,11 +120,11 @@ function init() {
   var clipboard = new ClipboardJS("#btn-copy", {
     text: trigger => $("#input-result").val()
   });
-  clipboard.on("success", function(e) {
+  clipboard.on("success", function (e) {
     setTooltip(e.trigger, "Copied!");
     hideTooltip(e.trigger);
   });
-  clipboard.on("error", function(e) {
+  clipboard.on("error", function (e) {
     setTooltip(e.trigger, "Failed!");
     hideTooltip(e.trigger);
   });
@@ -98,7 +145,16 @@ function init() {
     }
   });
 
-  $("#btn-gen").click(genPassword);
+  $("#btn-gen").click(() => {
+    let password = genPassword(
+      $("#input-domain").val().toLowerCase(),
+      $("#input-password").val()
+    );
+    $("#input-result").val(password);
+    showPassword("#btn-eye-result");
+    setTimeout(() => hidePassword("#btn-eye-result"), 500);
+    $("#btn-copy").click();
+  });
   $("form").submit(() => false);
 }
 
